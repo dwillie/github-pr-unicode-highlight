0c817dabfa61e295b16176bc3c88feaef6d9dc0a
David Willie <david.willie.1@gmail.com>
2018-01-25 11:29:28 +1100

    Work on an interval to ensure lazy loaded diffs are processed

--- pr-check.js
+++ pr-check.js
@@ -1,6 +1,15 @@
-const elems = document.getElementsByClassName("blob-code-addition");
-const unicodeRegex = /([^\x00-\x7F])+/g;
+const handledElems = [];
 
-for (var i = 0; i < elems.length; i++) {
-    elems[i].innerHTML = elems[i].innerHTML.replace(unicodeRegex, "<span style=\"min-width: 3px; background-color: red;\">$&</span>"); 
-}
+setInterval(function() {
+    const elems = document.getElementsByClassName("file");
+    const unicodeRegex = /([^\x00-\x7F])+/g;
+
+    for (var i = 0; i < elems.length; i++) {
+        if (handledElems.indexOf(elems[i].id) > -1) {
+            continue;
+        }
+
+        elems[i].innerHTML = elems[i].innerHTML.replace(unicodeRegex, "<span style=\"min-width: 3px; background-color: red;\">$&</span>"); 
+        handledElems.push(elems.id);
+    }
+}, 1000);
