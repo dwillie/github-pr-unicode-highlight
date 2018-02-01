08eef1218bb149f23702296ecfe5197a5af6383d
David Willie <david.willie.1@gmail.com>
2018-02-01 18:54:10 +1100

    fix false unicode character capture

--- pr-check.js
+++ pr-check.js
@@ -1,15 +1,15 @@
 const handledElems = [];
 
 setInterval(function() {
-    const elems = document.getElementsByClassName("file");
+    elems = document.getElementsByClassName("blob-code-inner");
     const unicodeRegex = /([^\x00-\x7F])+/g;
 
     for (var i = 0; i < elems.length; i++) {
-        if (handledElems.indexOf(elems[i].id) > -1) {
+        if (handledElems.indexOf(elems[i]) > -1) {
             continue;
         }
 
         elems[i].innerHTML = elems[i].innerHTML.replace(unicodeRegex, "<span style=\"min-width: 3px; background-color: red;\">$&</span>"); 
-        handledElems.push(elems.id);
+        handledElems.push(elems[i]);
     }
 }, 1000);
